4fb5bac1b73a3bc82931f5ddc43f32871be0aaba
julius-boettger <julius.btg@proton.me>
2024-05-27 08:34:49 +0200

    feat: add dark mode toggle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
     import Logo from "$lib/assets/logo-full-white.svg";
+    import { LightSwitch } from "@skeletonlabs/skeleton";
 </script>
 
 <svelte:head>
@@ -7,6 +8,7 @@
 </svelte:head>
 
 <div class="flex flex-col justify-center items-center h-screen">
+    <LightSwitch />
     <img src={Logo} alt="logo" class="w-500" />
     <h1 class="h1 text-center">Rent a car!</h1>
 </div>
