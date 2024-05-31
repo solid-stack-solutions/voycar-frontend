e597773b5be4115c6a0546b0cce4cbab3c9ab851
julius-boettger <julius.btg@proton.me>
2024-05-31 08:48:14 +0200

    refac: prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,14 @@
 /** @type {import("prettier").Config} */
 const config = {
-    // defaults we might want to change later
-    trailingComma: "all",
-    printWidth: 80,
     tabWidth: 4,
 
+    //// defaults we might want to change later
+    trailingComma: "all",
+    printWidth: 80,
+    // https://prettier.io/blog/2018/11/07/1.15.0#whitespace-sensitive-formatting
+    htmlWhitespaceSensitivity: "css",
+
+    // plugins and settings for file types
     plugins: ["prettier-plugin-svelte"],
     overrides: [{ files: "*.svelte", options: { parser: "svelte" } }],
 };
