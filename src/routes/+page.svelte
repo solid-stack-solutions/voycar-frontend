aa07273f5ca482b4482baa3d9146a28acd19b721
Jannik <jannik.hennicke@btc-ag.com>
2024-06-01 22:03:51 +0200

    feat: add testing landing page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<!-- Landing Page-->
+<script>
+	let name = 'Jannik';
+</script>
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-5">
+		<h1>Hello {name}</h1>
+	</div>
+</div>
