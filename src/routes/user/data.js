cb9fda9b9f6e66f74e42983c5e7c95c412fa4cf7
Janniix <jannik.hennicke@btc-ag.com>
2024-06-02 20:22:26 +0200

    test: add test userdata

---
+++ src/routes/user/data.js
@@ -0,0 +1,38 @@
+//Testdaten die später aus der Datenbank kommen
+export const users = [
+    {
+        userID : 1,
+        firstName : 'Peter',
+        lastName : 'Parker',
+        RolleID : 0,
+        email : 'peter.parker@mail.com',
+        street : 'Marx Street',
+        houseNumber : 123,
+        plz : 22334,
+        city : 'New York'
+    },
+
+    {
+        userID : 2,
+        firstName : 'Tony',
+        lastName : 'Stark',
+        RolleID : 1,
+        email : 'tony.stark@mail.com',
+        street : 'Tony Tower Street',
+        houseNumber : 1,
+        plz : 846789,
+        city : 'New York'
+    },
+    
+    {
+        userID : 3,
+        firstName : 'Bruce',
+        lastName : 'Wayne',
+        RolleID : 0,
+        email : 'bruce.vayne@mail.com',
+        street : 'Bat Street',
+        houseNumber : 420,
+        plz : 12312,
+        city : 'Gotham'
+    },
+]
