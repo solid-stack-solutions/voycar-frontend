d23f0b49c8a69670cee58ef51f22f9ced51c0371
Janniix <jannik.hennicke@btc-ag.com>
2024-06-03 13:11:28 +0200

    feat: add menu incon to AppBar, center footer text

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -25,12 +25,17 @@
 	import { page } from '$app/stores'; // hat automatisch alle Pages als Stores
 	import { AppBar } from '@skeletonlabs/skeleton'; // App Bar
 </script>
-
 <AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
-	<svelte:fragment slot="lead">(icon)</svelte:fragment>
+	<svelte:fragment slot="lead">
+		<svg class="h-8 w-8 text-slate-100"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  
+			<line x1="3" y1="12" x2="21" y2="12" />  
+			<line x1="3" y1="6" x2="21" y2="6" />  
+			<line x1="3" y1="18" x2="21" y2="18" />
+		</svg>
+	</svelte:fragment>
 	Voycar
 	<svelte:fragment slot="trail">
-		<a href="/user">
+		<a href="/user"> <!--UserIcon-->
 			<svg class="h-8 w-8 text-slate-100"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  
 				<path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2" />  
 				<circle cx="12" cy="7" r="4" />
@@ -39,9 +44,10 @@
 	</svelte:fragment>
 </AppBar>
 
+
 <TabGroup>
 	<TabAnchor href="/" selected={$page.url.pathname === '/'}>Home</TabAnchor>
 	<TabAnchor href="/reservations" selected={$page.url.pathname === '/reservations'}>Reservierungen</TabAnchor>
 </TabGroup>	
 <slot />
-<footer>Support: telfonnummer</footer>	
+<footer style="text-align: center;">Support: telefonnummer</footer>	
