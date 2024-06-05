48071b32a83fd16c06a06154499fbf5ec9faa083
Janniix <jannik.hennicke@btc-ag.com>
2024-06-05 19:11:45 +0200

    refac: reformatted

--- src/routes/user/[userID]/+page.js
+++ src/routes/user/[userID]/+page.js
@@ -1,12 +1,12 @@
 import { users } from "../data";
-import { error } from '@sveltejs/kit';
+import { error } from "@sveltejs/kit";
 
 export function load({ params }) {
-	const user = users.find((user) => user.userID.toString() === params.userID);
+    const user = users.find((user) => user.userID.toString() === params.userID);
 
-	if (!user) throw error(404);
+    if (!user) throw error(404);
 
-	return {
-		user
-	};
+    return {
+        user,
+    };
 }
