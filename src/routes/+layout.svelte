8a2630938d4a949c0b7c1593a131d2a3aaea2956
Janniix <jannik.hennicke@btc-ag.com>
2024-06-08 23:58:08 +0200

    feat: use svg file in footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -69,18 +69,10 @@
 
 <slot />
 
-<footer style="text-align: center;">
-    Support: telefonnummer
-    <svg 
-    class="h-4 w-4"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke="currentColor">
-    <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
-    />
-</svg>
+<footer style="text-items: center;">
+    <div style="display: flex; align-text: center;">
+        Support:
+        <img src="/telephoneIcon.svg" alt="phone icon" style="margin-right: 4px; margin-left: 4px;">
+        +49 123 42018769      
+    </div>
 </footer>
