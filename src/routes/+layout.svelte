4077a849d37d25d701c5f3c81703794a890cee7f
Janniix <jannik.hennicke@btc-ag.com>
2024-06-08 16:02:07 +0200

    feat: add flaotiong ui import and setup to root layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,9 @@
     import { TabGroup, Tab, TabAnchor } from "@skeletonlabs/skeleton"; //Menüleiste mit auswählbaren Tabs
     import { page } from "$app/stores"; // hat automatisch alle Pages als Stores
     import { AppBar } from "@skeletonlabs/skeleton"; // App Bar
-
+    import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
+    import { storePopup } from '@skeletonlabs/skeleton';
+    storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
     import Logo from "$lib/assets/logo-full-white.svg";
     // these are skeleton svelte components!
     import { LightSwitch } from "@skeletonlabs/skeleton";
@@ -68,17 +70,17 @@
 <slot />
 
 <footer style="text-align: center;">
-    <svg 
-        class="h-4 w-4"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor">
-        <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
-        />
-    </svg>
     Support: telefonnummer
+    <svg 
+    class="h-4 w-4"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor">
+    <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
+    />
+</svg>
 </footer>
