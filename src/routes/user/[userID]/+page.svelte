7b6cb838c531b7d4aeb438ee5de9b9f6f82efccc
Janniix <jannik.hennicke@btc-ag.com>
2024-06-10 20:19:02 +0200

    feat: remove currently unused and not working redirect

--- src/routes/user/[userID]/+page.svelte
+++ src/routes/user/[userID]/+page.svelte
@@ -1,13 +1,8 @@
 <script>
-    import { redirect } from "@sveltejs/kit";
     import LoggedInUser from "./loggedInUser.svelte";
     export let data;
-    if (data == null) {
-        //doesnt yet work
-        redirect(300, "/");
-    }
 </script>
 
-{#if { data } != null}
+{#if data != null}
     <LoggedInUser {data}></LoggedInUser>
 {/if}
