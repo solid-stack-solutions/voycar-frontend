0d14eea0f660b4aa543646856dbda6b1b7a3789c
Janniix <jannik.hennicke@btc-ag.com>
2024-06-10 19:51:25 +0200

    feat: add plan data page content

--- src/routes/user/[userID]/userPlanData.svelte
+++ src/routes/user/[userID]/userPlanData.svelte
@@ -1,14 +1,17 @@
 <script>
     export let userID;
+    // geht erst wenn Datanbankverbindung vorhanden
     // let userPlanData = fetchUserPlanData(userId);
 
     // async function fetchUserPlanData(userId){
-    //     return fetch();
+    //     myReq = new Request()
     // }
 </script>
-<div class="static h-8">
+<div>
     Aktueller Tarif:
     <!-- hier dann noch daten abrufen und tarif einfügen -->
-     {userID}
-    <span>
+     <!-- {#if userPlanData != null} -->
+     {userID}  
+     <!-- {/if} -->
+      <button class="btn text-warning-500">Tarif ändern</button>
 </div>
