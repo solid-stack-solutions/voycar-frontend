c0f7980159eab5e28031ca06928263e898f609da
Janniix <jannik.hennicke@btc-ag.com>
2024-06-10 14:54:59 +0200

    feat: modularize user payment data

---
+++ src/routes/user/[userID]/userPaymentData.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let userID;
+    // let userPaymentData = fetchUserPaymetData(userId);
+
+    // async function fetchUserPaymetData(userId){
+    //     return fetch();
+    // }
+</script>
+<div class="static h-8">
+    Zahlungsinformation
+    <!-- hier dann noch daten abrufen und tarif einfügen -->
+     {userID}
+    <span>
+</div>
