d3628d3e962c9228f477e558b5e87f50ced62c5c
Janniix <jannik.hennicke@btc-ag.com>
2024-06-10 14:55:15 +0200

    feat: modularize user plan data

---
+++ src/routes/user/[userID]/userPlanData.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let userID;
+    // let userPlanData = fetchUserPlanData(userId);
+
+    // async function fetchUserPlanData(userId){
+    //     return fetch();
+    // }
+</script>
+<div class="static h-8">
+    Aktueller Tarif:
+    <!-- hier dann noch daten abrufen und tarif einfügen -->
+     {userID}
+    <span>
+</div>
