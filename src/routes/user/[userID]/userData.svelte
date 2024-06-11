8a991f1a55e1ab8de8f29f0077637f2828f8760b
Janniix <jannik.hennicke@gmail.com>
2024-06-11 14:26:25 +0200

    feat: remove border from table

--- src/routes/user/[userID]/userData.svelte
+++ src/routes/user/[userID]/userData.svelte
@@ -3,7 +3,7 @@
     export let userInfo;
     let formEditEnabled = false;
 </script>
-<div class="table-container border-4 rounded-md border-primary-500 p-2">
+<div class="table-container rounded-md p-2">
     <table class="table table-fixed border-4 rounded-md w-full"> <!--tbh idk why the hover effect doesnt work -->
         {#each userKeys as key, i}
             {#if userInfo[i] != null}  <!-- only show rows with information available -->
