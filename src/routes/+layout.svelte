f93b97fc5409f415b8731eec19e6df2d624b91fc
Janniix <jannik.hennicke@gmail.com>
2024-06-12 22:28:23 +0200

    feat: modularize svgs

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -23,19 +23,7 @@
     slotTrail="place-content-end"
 >
     <svelte:fragment slot="lead">
-        <svg
-            class="h-8 w-8"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-        >
-            <line x1="3" y1="12" x2="21" y2="12" />
-            <line x1="3" y1="6" x2="21" y2="6" />
-            <line x1="3" y1="18" x2="21" y2="18" />
-        </svg>
+        <img src="/menuIcon.svg" alt="menu icon">
     </svelte:fragment>
     <svelte:fragment slot="default">
         <a href="/">
@@ -46,18 +34,7 @@
     <svelte:fragment slot="trail">
         <a href="/user">
             <!-- User icon -->
-            <svg
-                class="h-8 w-8"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-            >
-                <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2" />
-                <circle cx="12" cy="7" r="4" />
-            </svg>
+            <img src="/userIcon.svg" alt="user icon">
         </a>
         <LightSwitch />
     </svelte:fragment>
--- src/routes/user/[userID]/userPaymentData.svelte
+++ src/routes/user/[userID]/userPaymentData.svelte
@@ -58,40 +58,13 @@
             {#if formEditEnabled}
                 verlassen
             {/if}
-            <svg
-                class="h-4 w-4"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                stroke-width="2"
-                stroke="currentColor"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-            >
-                <path stroke="none" d="M0 0h24v24H0z" />
-                <path d="M4 20h4l10.5 -10.5a1.5 1.5 0 0 0 -4 -4l-10.5 10.5v4" />
-                <line x1="13.5" y1="6.5" x2="17.5" y2="10.5" /></svg
-            >
+            <img src="/editIcon.svg" alt="edit icon">
         </button>
         {#if formEditEnabled}
             <button type="button" class="btn btn-md variant-filled-success">
                 <!-- ToDo logic for update button -->
                 Aktualisieren
-                <svg
-                    class="h-4 w-4"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                >
-                    <path
-                        d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z"
-                    /> <polyline points="17 21 17 13 7 13 7 21" />
-                    <polyline points="7 3 7 8 15 8" /></svg
-                >
+                <img src="/saveIcon.svg" alt="save icon">
             </button>
         {/if}
     </div>
