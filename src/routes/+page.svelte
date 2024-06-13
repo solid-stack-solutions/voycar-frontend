3b7cd6122decd3b56d566826319c23645e63960c
Janniix <jannik.hennicke@gmail.com>
2024-06-13 20:21:57 +0200

    feat: remove unused paragraph

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,5 @@
 <div class="flex flex-col justify-center items-center" style="height: 70vh;">
     <div class="border-md rounded-md bg-surface-700 p-4">
         <h1 class="h1 text-center">Willkommen bei Voycar</h1>
-        <p class="text-block"></p>
     </div>
 </div>
