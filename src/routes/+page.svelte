22057fde390da865f2e1fa373a80e21a1959b033
Janniix <jannik.hennicke@gmail.com>
2024-06-15 14:05:28 +0200

    feat: add arbitrary tailwind class value for height

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
     <title>Voycar</title>
 </svelte:head>
 
-<div class="flex flex-col justify-center items-center" style="height: 70vh;">
+<div class="flex flex-col justify-center items-center h-[70vh]">
     <div class="border-md rounded-md bg-surface-700 p-4">
         <h1 class="h1 text-center">Willkommen bei Voycar</h1>
     </div>
