ce108faac5e36c76a5eb97830b1e5f8255cf2dfa
Janniix <jannik.hennicke@gmail.com>
2024-06-18 19:31:52 +0200

    feat: change placeholder layout to match data layout

--- src/routes/user/userPageLoadingPlaceholders.svelte
+++ src/routes/user/userPageLoadingPlaceholders.svelte
@@ -1,9 +1,9 @@
-<div class="relative pt-4">
-    <div class="mb-4 ml-4 placeholder animate-pulse w-60 h-7" />
-    <div class="p-4 pt-4 space-y-6">
+<div class="relative pt-1">
+    <div class="mb-4 placeholder animate-pulse w-60 h-8" />
+    <div class="space-y-2">
         {#each {length: 3} as _}
-            <div class="placeholder animate-pulse h-6"></div>            
+            <div class="placeholder animate-pulse h-10"></div>            
         {/each}
     </div>
-    <div class="placeholder animate-pulse w-60 h-10 absolute right-4"/>
+    <div class="placeholder animate-pulse w-60 h-12 absolute right-0 mt-2"/>
 </div>
