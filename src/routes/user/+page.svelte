2e1601ffddec3108f132268cd5bded47eac4945a
Janniix <jannik.hennicke@gmail.com>
2024-06-18 13:16:49 +0200

    feat: modularize placeholders as own svelte component

--- src/routes/user/+page.svelte
+++ src/routes/user/+page.svelte
@@ -4,6 +4,7 @@
     import { onMount } from "svelte";
     // Util library import for url routes
     import { urls } from "$lib/util.js";
+    import UserPageLoadingPlaceholders from "./userPageLoadingPlaceholders.svelte";
 
     // Definitions
     let loading = true;
@@ -39,27 +40,7 @@
 {#if loading}
   <p class="text-center h3">Warten auf Weiterleitung zu Ihrer Kontoseite</p>
   <!-- Placeholders -->
-   <div>
-    <div class="placeholder animate-pulse pb-4 pl-2 w-60 h-6"></div>
-    <div class="p-4 space-y-6">
-		<div class="grid grid-cols-2 gap-8">
-            {#each {length: 10} as _,i }
-            <div class="placeholder animate-pulse h-6" />
-            {/each}
-		</div>
-		<div class="grid grid-cols-4 gap-4">
-			{#each {length: 4} as _,i }
-            <div class="placeholder animate-pulse h-4" />
-            {/each}
-		</div>
-        {#each {length: 2} as _,i }
-            <div class="grid grid-cols-4 gap-4">
-                <div class="placeholder animate-pulse" />
-                <div class="placeholder animate-pulse" />
-            </div>
-        {/each}
-    </div>
-   </div>
+   <UserPageLoadingPlaceholders/>
 {:else if error}
   <p>Fehler: {error}</p>
 {:else}
---
+++ src/routes/user/userPageLoadingPlaceholders.svelte
@@ -0,0 +1,21 @@
+<div>
+    <div class="placeholder animate-pulse pb-4 pl-2 w-60 h-6"></div>
+    <div class="p-4 space-y-6">
+		<div class="grid grid-cols-2 gap-8">
+            {#each {length: 10} as _,i }
+            <div class="placeholder animate-pulse h-6" />
+            {/each}
+		</div>
+		<div class="grid grid-cols-4 gap-4">
+			{#each {length: 4} as _,i }
+            <div class="placeholder animate-pulse h-4" />
+            {/each}
+		</div>
+        {#each {length: 2} as _,i }
+            <div class="grid grid-cols-4 gap-4">
+                <div class="placeholder animate-pulse" />
+                <div class="placeholder animate-pulse" />
+            </div>
+        {/each}
+    </div>
+   </div>
