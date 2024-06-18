72b4738c0c5ee2d52c8ba89844aa44377d028e0d
Janniix <jannik.hennicke@gmail.com>
2024-06-18 13:11:34 +0200

    feat: add backend fetch logic

--- src/routes/user/+page.svelte
+++ src/routes/user/+page.svelte
@@ -2,18 +2,36 @@
     // Framework imports
     import { goto } from "$app/navigation";
     import { onMount } from "svelte";
+    // Util library import for url routes
+    import { urls } from "$lib/util.js";
+
+    // Definitions
+    let loading = true;
+    let data = null;
+    let error = null;
 
     // Functions
-    function fetchUserID() {
-        // ToDo fetch user id from backend
-        return 1;
-    }
-
     function redirectToUserpage(id) {
         goto(`/user/${id}`);
     }
 
-    onMount(() => {
-        redirectToUserpage(fetchUserID());
+    // ToDo: implement auto refetching if fetch failed
+
+    // Runs as soon as this component is mounted
+    onMount(async () => {
+    try {
+      const response = await fetch(urls.get.UserID);
+      if (response.ok) {
+        userID = await response.json();
+        loading = false;
+        // Redirect user when data was fetched
+        redirectToUserpage(data.UserID)
+      } else {
+        throw new Error('Fehler beim Laden der Daten');
+      }
+    } catch (err) {
+      error = err.message;
+      loading = false;
+    }
     });
 </script>
