46c3c557633b20f72f62019f5fe609fd67abb4e1
Janniix <jannik.hennicke@gmail.com>
2024-06-18 21:43:26 +0200

    feat: add toast

--- src/routes/user/+page.svelte
+++ src/routes/user/+page.svelte
@@ -1,33 +1,38 @@
 <script>
     // Framework imports
+    import { getToastStore } from '@skeletonlabs/skeleton';
     import { goto } from "$app/navigation";
     import { onMount } from "svelte";
     // Util library import for url routes
     import { urls } from "$lib/util.js";
     import UserPageLoadingPlaceholders from "./userPageLoadingPlaceholders.svelte";
-    import loggedInUser from "./loggedInUser.svelte"
-     
+    import LoggedInUser from "./loggedInUser.svelte";
+
     // Definitions
+    const toastStore = getToastStore();
     let loading = true;
-    let data = null;
     let error = null;
+    let data = null;
 
+    // Toast settings
+    const t = {
+      message: 'Ihr Nutzerkonto konnte nicht gefunden werden',
+      hideDismiss: true,
+      timeout: 3000,
+      background: 'variant-filled-secondary'
+    };
+    
     // Functions
-    function redirectToUserpage(id) {
-    goto(`/user/${id}`);
-    }
-
     // ToDo: implement auto refetching if fetch failed
 
     // Runs as soon as this component is mounted
     onMount(async () => {
     try {
-      const response = await fetch(urls.get.UserID);
+      const response = await fetch(urls.get.memberData);
       if (response.ok) {
-        userID = await response.json();
+        // data = await response.json();
         loading = false;
         // Redirect user when data was fetched
-        redirectToUserpage(data.UserID)
       } else {
         throw new Error('Fehler beim Laden der Daten');
       }
@@ -35,22 +40,19 @@
       error = err.message;
       loading = false;
       // question: do we want to redirect the user instantly without letting them know what happend or display a message for 1-2 seconds and then redirect?
-      setTimeout(() => {
-        goto("/");
-      }, 1000);
-      
+      toastStore.trigger(t);
+      goto("/");
     }
   });
 </script>
 <div>
     {#if loading}
-        <p class="text-center h3">Warten auf Weiterleitung zu Ihrer Kontoseite</p>
     <!-- Placeholders -->
     <UserPageLoadingPlaceholders/>
     {:else if error}
-        <p class="text-center h3">Ihr User konnte nicht gefunden werden. Sie werden auf die Startseite zurückgeleitet!</p>
+        <p class="text-center h3">Ihr Nutzerkonto konnte nicht gefunden werden. Sie werden auf die Startseite zurückgeleitet!</p>
         <p class="text-center">Fehler: {error}</p>
     {:else}
-        
+      <LoggedInUser {data}></LoggedInUser>  
     {/if}
 </div>
