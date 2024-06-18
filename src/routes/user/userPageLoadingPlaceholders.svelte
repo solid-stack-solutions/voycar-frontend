8569b9b828afdb952a90dbae03eb3f7675eb0ba2
Janniix <jannik.hennicke@gmail.com>
2024-06-18 14:13:15 +0200

    feat: fix placeholder width, height and spacing

--- src/routes/user/userPageLoadingPlaceholders.svelte
+++ src/routes/user/userPageLoadingPlaceholders.svelte
@@ -1,21 +1,9 @@
-<div>
-    <div class="placeholder animate-pulse pb-4 pl-2 w-60 h-6"></div>
-    <div class="p-4 space-y-6">
-		<div class="grid grid-cols-2 gap-8">
-            {#each {length: 10} as _,i }
-            <div class="placeholder animate-pulse h-6" />
-            {/each}
-		</div>
-		<div class="grid grid-cols-4 gap-4">
-			{#each {length: 4} as _,i }
-            <div class="placeholder animate-pulse h-4" />
-            {/each}
-		</div>
-        {#each {length: 2} as _,i }
-            <div class="grid grid-cols-4 gap-4">
-                <div class="placeholder animate-pulse" />
-                <div class="placeholder animate-pulse" />
-            </div>
+<div class="relative pt-4">
+    <div class="mb-4 ml-4 placeholder animate-pulse w-60 h-7" />
+    <div class="p-4 pt-4 space-y-6">
+        {#each {length: 3} as _}
+            <div class="placeholder animate-pulse h-6"></div>            
         {/each}
     </div>
-   </div>
+    <div class="placeholder animate-pulse w-60 h-10 absolute right-4"/>
+</div>
