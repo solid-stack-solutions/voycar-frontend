70ce6e6bfa9c4c5809f54da0550a60c0afe29266
Janniix <jannik.hennicke@gmail.com>
2024-06-18 13:12:09 +0200

    feat: add placeholder bars while waiting for fetched data

--- src/routes/user/+page.svelte
+++ src/routes/user/+page.svelte
@@ -12,7 +12,7 @@
 
     // Functions
     function redirectToUserpage(id) {
-        goto(`/user/${id}`);
+    goto(`/user/${id}`);
     }
 
     // ToDo: implement auto refetching if fetch failed
@@ -33,5 +33,36 @@
       error = err.message;
       loading = false;
     }
-    });
+  });
 </script>
+<div>
+{#if loading}
+  <p class="text-center h3">Warten auf Weiterleitung zu Ihrer Kontoseite</p>
+  <!-- Placeholders -->
+   <div>
+    <div class="placeholder animate-pulse pb-4 pl-2 w-60 h-6"></div>
+    <div class="p-4 space-y-6">
+		<div class="grid grid-cols-2 gap-8">
+            {#each {length: 10} as _,i }
+            <div class="placeholder animate-pulse h-6" />
+            {/each}
+		</div>
+		<div class="grid grid-cols-4 gap-4">
+			{#each {length: 4} as _,i }
+            <div class="placeholder animate-pulse h-4" />
+            {/each}
+		</div>
+        {#each {length: 2} as _,i }
+            <div class="grid grid-cols-4 gap-4">
+                <div class="placeholder animate-pulse" />
+                <div class="placeholder animate-pulse" />
+            </div>
+        {/each}
+    </div>
+   </div>
+{:else if error}
+  <p>Fehler: {error}</p>
+{:else}
+  <p>Daten geladen, Sie werden weitergeleitet...</p>
+{/if}
+</div>
