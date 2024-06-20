955dc964d79edada82171a0d742e42d460c55bc8
Janniix <jannik.hennicke@gmail.com>
2024-06-20 19:30:41 +0200

    feat: add whomai route

--- src/lib/util.js
+++ src/lib/util.js
@@ -1,6 +1,12 @@
 // helper functions that can easily be imported anywhere go here
 // Routing strings
-const url = {
-    memberInfo: "localhost:8080/....",
-    // ...
+const baseURL = "http://localhost:8080/";
+export const urls = {
+    get: {
+        memberPersonalData: baseURL + "member/personal",
+        isLoggedIn: baseURL + "user/whoami"
+    },
+    post: {},
+    put: {},
+    delete: {},
 };
