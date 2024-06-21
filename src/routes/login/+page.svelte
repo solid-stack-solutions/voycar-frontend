3b13627945465d304c9f14ac2fe3aa74309b870f
Janniix <jannik.hennicke@gmail.com>
2024-06-21 17:22:29 +0200

    feat: change border-width, change email placeholder

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,25 +1,25 @@
 <div class="flex flex-col justify-center items-center mt-4">
     <h1 class="h2 mb-8">Bei Voycar anmelden</h1>
     <div class="w-96 justify-center items-center space-y-4">
-        <form class="p-4 border-4 rounded-md border-secondary-500 space-y-3">
+        <form class="p-4 border-2 rounded-md border-secondary-500 space-y-3">
             <!-- Email field -->
                 <label class="label" for="email_input">
                     <span>Email</span>
                 </label>
-            <input class="input" type="text" id="email_input" placeholder="example.org@mail.com"/>
+            <input class="input" type="text" id="email_input" placeholder="beispiel.organisation@mail.com"/>
             <!-- Password field -->
             <label class="label" for="password_input">
                 <span>Passwort</span>
             </label>
-            <input class="input" type="password" id="password_input" placeholder="Ihr super sicheres Passwort 😉"/>
+            <input class="input" type="password" id="password_input" placeholder="Dein super sicheres Passwort 😉"/>
             <div class="flex flex-col items-center">
                 <button class="btn variant-filled-primary w-full">Anmelden</button>
             </div>
             <div class="flex justify-center items-center">
-                <a class="text-sm text-tertiary-600" href="/register">Passwort vergessen?</a>
+                <a class="text-sm text-tertiary-600" href="/forgotPassword">Passwort vergessen?</a>
             </div>
         </form>
-        <div class="flex flex-col-2 items-center justify-between border-4 rounded-md border-secondary-500 p-4">
+        <div class="flex flex-col-2 items-center justify-between border-2 rounded-md border-secondary-500 p-4">
             <p>Neu bei Voycar?</p>
             <a class="text-tertiary-500" href="/register">Neuen Account erstellen</a>
         </div>
