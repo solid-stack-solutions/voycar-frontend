d1d497fbbad1764b66da190b9e50187b181c8809
Janniix <jannik.hennicke@gmail.com>
2024-06-21 17:13:24 +0200

    feat: add backend routings

--- src/lib/util.js
+++ src/lib/util.js
@@ -1,11 +1,20 @@
 // Helper functions that can easily be imported anywhere go here
 // Routing strings
 const baseURL = "http://localhost:8080/";
+
+const member = "member/";
+const auth = "auth/";
+
 export const urls = {
     get: {
-        memberPersonalData: baseURL + "member/personal",
+        memberPersonalData: baseURL + member + "personal",
     },
-    post: {},
+    post: {
+        forgotPassword: baseURL + auth + "forgot-password",
+        register: baseURL + auth + "register",
+        login: baseURL + auth + "login",
+        logout: baseURL + auth + "logout"
+    },
     put: {},
     delete: {},
 };
