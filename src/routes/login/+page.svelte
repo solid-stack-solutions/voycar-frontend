9a5d10fe56c6ffb7532ba0c1e60d33da2e45f6b6
Janniix <jannik.hennicke@gmail.com>
2024-06-21 16:42:02 +0200

    feat: add login page

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,27 @@
+<div class="flex flex-col justify-center items-center mt-4">
+    <h1 class="h2 mb-8">Bei Voycar anmelden</h1>
+    <div class="w-96 justify-center items-center space-y-4">
+        <form class="p-4 border-4 rounded-md border-secondary-500 space-y-3">
+            <!-- Email field -->
+                <label class="label" for="email_input">
+                    <span>Email</span>
+                </label>
+            <input class="input" type="text" id="email_input" placeholder="example.org@mail.com"/>
+            <!-- Password field -->
+            <label class="label" for="password_input">
+                <span>Passwort</span>
+            </label>
+            <input class="input" type="password" id="password_input" placeholder="Ihr super sicheres Passwort 😉"/>
+            <div class="flex flex-col items-center">
+                <button class="btn variant-filled-primary w-full">Anmelden</button>
+            </div>
+            <div class="flex justify-center items-center">
+                <a class="text-sm text-tertiary-600" href="/register">Passwort vergessen?</a>
+            </div>
+        </form>
+        <div class="flex flex-col-2 items-center justify-between border-4 rounded-md border-secondary-500 p-4">
+            <p>Neu bei Voycar?</p>
+            <a class="text-tertiary-500" href="/register">Neuen Account erstellen</a>
+        </div>
+    </div>
+</div>
---
+++ src/routes/user/+page.js
@@ -0,0 +1,8 @@
+// Imports
+import { users } from "./data";
+
+// Functions
+// Load user data
+export function load() {
+    return users[0]; //only for testing
+}
