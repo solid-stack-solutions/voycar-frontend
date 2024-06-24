551905f0b400ed8c7b890009af61578dde2c5e0c
NilsPvR <nilspvr@pm.me>
2024-06-24 14:05:52 +0200

    fix: incorrect route for whoami endpoint

--- src/lib/util.js
+++ src/lib/util.js
@@ -2,13 +2,14 @@
 // Routing strings
 const baseURL = "http://localhost:8080/";
 
+const user = "user/"
 const member = "member/";
 const auth = "auth/";
 
 export const urls = {
     get: {
         memberPersonalData: baseURL + member + "personal",
-        isLoggedIn: baseURL + member + "whoami",
+        isLoggedIn: baseURL + user + "whoami",
     },
     post: {
         forgotPassword: baseURL + auth + "forgot-password",
