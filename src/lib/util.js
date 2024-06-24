443fbb4d372c715ca92e8f970c26d107b1726130
Janniix <jannik.hennicke@gmail.com>
2024-06-24 16:28:45 +0200

    feat: add backend reservation url

--- src/lib/util.js
+++ src/lib/util.js
@@ -1,9 +1,11 @@
 // Helper functions that can easily be imported anywhere go here
 // Routing strings
 const baseURL = "http://localhost:8080/";
+const reservation = "reservation/";
 export const urls = {
     get: {
         memberPersonalData: baseURL + "member/personal",
+        reservationPersonalData : baseURL + reservation + "personal",
     },
     post: {},
     put: {},
