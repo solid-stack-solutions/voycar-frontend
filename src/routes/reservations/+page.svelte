90451055cf5445459cf40337a7a11dfc8e67d1e9
Janniix <jannik.hennicke@gmail.com>
2024-06-24 23:27:28 +0200

    feat: add Title Head

--- src/routes/reservations/+page.svelte
+++ src/routes/reservations/+page.svelte
@@ -4,14 +4,14 @@
     import { ExponentialBackoff, handleAll, retry } from "cockatiel";
     import { urls } from "$lib/util.js";
     import { Accordion, AccordionItem } from "@skeletonlabs/skeleton";
-    import { ProgressRadial } from "@skeletonlabs/skeleton";    
+    import { ProgressRadial } from "@skeletonlabs/skeleton";
     // Definitions
     // Policy for restarting backend fetched up to 5 times if there's no reply
     const retryPolicy = retry(handleAll, {
         maxAttempts: 5,
         backoff: new ExponentialBackoff(),
     });
-    
+
     const test = {
         begin: "2024-06-21T19:11:46.833Z",
         end: "2024-09-21T19:11:46.833Z",
@@ -21,9 +21,7 @@
     };
 
     function filterDate(dateString) {
-        return new Intl.DateTimeFormat("de-DE").format(
-            new Date(dateString),
-        );
+        return new Intl.DateTimeFormat("de-DE").format(new Date(dateString));
     }
 
     let reservationData = new Promise((resolve, reject) => {});
@@ -45,43 +43,68 @@
         });
     });
 </script>
+<svelte:head>
+    <title>Reservierungen</title>
+</svelte:head>
 
 <div class="relative">
-    {#await reservationData}
-    <div class="flex h-[70vh] flex-col items-center justify-center space-y-4">
-        <h2 class="h2"> Laden deiner Reservierungen </h2>
-        <ProgressRadial stroke={60} meter="stroke-primary-500" track="stroke-primary-500/30" strokeLinecap="butt"/>
-    </div>
-    {:then reservations}
-    <Accordion>
-        <div>
-            {#each { reservations } as reservation}
-            <AccordionItem>
-                <svelte:fragment slot="summary">Reservierung für den {reservation.begin}</svelte:fragment>
-                <svelte:fragment slot="content">
-                    <Reservation reservationData={reservation}></Reservation>
-                </svelte:fragment>
-            </AccordionItem>
-            {/each}
+    <div class="flex flex-col ">
+        <div class="border-md rounded-md bg-surface-700 p-4">
+            <h3 class="h3"> Ihre Reservierungen</h3>
         </div>
-    </Accordion>
+    </div>
+    <hr class="my-4 !border-t-4 rounded-full">
+    {#await reservationData}
+        <div
+            class="flex h-[70vh] flex-col items-center justify-center space-y-4"
+        >
+            <h2 class="h2">Laden deiner Reservierungen</h2>
+            <ProgressRadial
+                stroke={60}
+                meter="stroke-primary-500"
+                track="stroke-primary-500/30"
+                strokeLinecap="butt"
+            />
+        </div>
+    {:then reservations}
+        <Accordion>
+            <div>
+                {#each { reservations } as reservation}
+                    <AccordionItem class="rounded-lg  bg-surface-600">
+                        <svelte:fragment slot="summary"
+                            >Reservierung für den {filterDate(
+                                reservation.begin,
+                            )}</svelte:fragment
+                        >
+                        <svelte:fragment slot="content">
+                            <Reservation reservationData={reservation}
+                            ></Reservation>
+                        </svelte:fragment>
+                    </AccordionItem>
+                {/each}
+            </div>
+        </Accordion>
+        <div class="relative mt-2">
+            <button class="variant-filled-secondary btn absolute right-0"
+                >Neue reservierung erstellen</button
+            >
+        </div>
     {:catch error}
         <p>Reservierungen konnten nicht geladen werden</p>
     {/await}
     <!-- Testing -->
     <Accordion>
         <div>
-            <AccordionItem>
-                <svelte:fragment slot="summary">Reservierung für den {filterDate(test.begin)}</svelte:fragment>
+            <AccordionItem class="rounded-lg  bg-surface-600">
+                <svelte:fragment slot="summary"
+                    >Reservierung für den {filterDate(
+                        test.begin,
+                    )}</svelte:fragment
+                >
                 <svelte:fragment slot="content">
                     <Reservation reservationData={test}></Reservation>
                 </svelte:fragment>
             </AccordionItem>
         </div>
     </Accordion>
-    <div class="relative">
-        <button class="variant-filled-secondary btn absolute right-4"
-            >Neue reservierung erstellen</button
-        >
-    </div>
 </div>
