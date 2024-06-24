0be77e1089737ee5c2e194e919f353dca80fd8de
Janniix <jannik.hennicke@gmail.com>
2024-06-24 20:02:57 +0200

    feat: add accordion organisation for reservation

--- src/routes/reservations/+page.svelte
+++ src/routes/reservations/+page.svelte
@@ -1,8 +1,9 @@
 <script>
-    import { Accordion, AccordionItem } from "@skeletonlabs/skeleton";
     import Reservation from "./reservation.svelte";
     import { onMount } from "svelte";
     import { ExponentialBackoff, handleAll, retry } from "cockatiel";
+    import { urls } from "$lib/util.js";
+    import { Accordion, AccordionItem } from "@skeletonlabs/skeleton";
 
     // Definitions
     // Policy for restarting backend fetched up to 5 times if there's no reply
@@ -13,21 +14,26 @@
 
     const test = {
         begin: "2024-06-21T19:11:46.833Z",
-      end: "2024-09-21T19:11:46.833Z",
-      memberId: "5b3f5a8d-0e6c-4422-8ffa-d3b6f250c8a4",
-      carId: "bd980f9e-e049-486a-97d2-783e9158f2a7",
-      id: "50feffac-4d83-450a-913f-e816be805349"   
+        end: "2024-09-21T19:11:46.833Z",
+        memberId: "5b3f5a8d-0e6c-4422-8ffa-d3b6f250c8a4",
+        carId: "bd980f9e-e049-486a-97d2-783e9158f2a7",
+        id: "50feffac-4d83-450a-913f-e816be805349",
+    };
+
+    function filterDate(dateString) {
+        return new Intl.DateTimeFormat("de-DE").format(
+            new Date(dateString),
+        );
     }
 
     let error = "";
     let reservationData = new Promise((resolve, reject) => {});
-    let reservations = null;
     onMount(async () => {
         reservationData = new Promise(async (resolve, reject) => {
             try {
                 // Fetch backend for reservation Data with retry policy
                 const response = await retryPolicy.execute(() =>
-                    fetch(),
+                    fetch(urls.get.reservationPersonalData),
                 );
                 if (response.ok) {
                     reservations = resolve(await response.json());
@@ -44,19 +50,37 @@
 <div class="relative">
     {#await reservationData}
         <p class="animate-pulse">Loading</p>
-    {:then reservations} 
-    <div>
-        {#each {reservations} as reservation }
-            <Reservation reservationData={reservation}></Reservation>
-        {/each}
-    </div> 
+    {:then reservations}
+    <Accordion>
+        <div>
+            {#each { reservations } as reservation}
+            <AccordionItem>
+                <svelte:fragment slot="summary">Reservierung für den {reservation.begin}</svelte:fragment>
+                <svelte:fragment slot="content">
+                    <Reservation reservationData={reservation}></Reservation>
+                </svelte:fragment>
+            </AccordionItem>
+            {/each}
+        </div>
+    </Accordion>
     {:catch error}
-    <p>Reservierungen konnten nicht geladen werden</p>
-    {/await } 
-
+        <p>Reservierungen konnten nicht geladen werden</p>
+    {/await}
+    <Reservation reservationData={test}></Reservation>
+    <Accordion>
+        <div>
+            <AccordionItem>
+                <svelte:fragment slot="summary">Reservierung für den {filterDate(test.begin)}</svelte:fragment>
+                <svelte:fragment slot="content">
+                    <Reservation reservationData={test}></Reservation>
+                </svelte:fragment>
+            </AccordionItem>
+        </div>
+    </Accordion>
     <div class="relative">
-        <button class="btn variant-filled-secondary absolute right-4">Neue reservierung erstellen</button>
+        <button class="variant-filled-secondary btn absolute right-4"
+            >Neue reservierung erstellen</button
+        >
     </div>
     {error}
-    <Reservation reservationData={test}></Reservation>
 </div>
