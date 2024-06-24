1e6b2cc24e51c27d53ee1eae37f80ccc9b2fac0a
Janniix <jannik.hennicke@gmail.com>
2024-06-24 20:03:37 +0200

    feat: add personal data and car route

--- src/lib/util.js
+++ src/lib/util.js
@@ -2,10 +2,12 @@
 // Routing strings
 const baseURL = "http://localhost:8080/";
 const reservation = "reservation/";
+const car = "car/";
 export const urls = {
     get: {
         memberPersonalData: baseURL + "member/personal",
-        reservationPersonalData : baseURL + reservation + "personal",
+        reservationPersonalData: baseURL + reservation + "personal",
+        singleCar: baseURL + car,
     },
     post: {},
     put: {},
