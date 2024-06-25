5e767b0dbe36836a1c8fa9f42e38135fe4b9fad3
Janniix <jannik.hennicke@gmail.com>
2024-06-25 12:35:17 +0200

    feat: modulize reservation List

---
+++ src/routes/reservations/reservationList.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { AccordionItem } from "@skeletonlabs/skeleton";
+    import Reservation from "./reservation.svelte";
+    export let reservations;
+
+    function filterDate(dateString) {
+        return new Intl.DateTimeFormat("de-DE").format(new Date(dateString));
+    }
+    
+</script>
+<div>
+    {#each { reservations } as reservation}
+        <AccordionItem class="rounded-lg  bg-surface-600">
+            <svelte:fragment slot="summary"
+                >Reservierung für den {filterDate(
+                    reservation.begin,
+                )}</svelte:fragment
+            >
+            <svelte:fragment slot="content">
+                <Reservation reservationData={reservation}
+                ></Reservation>
+            </svelte:fragment>
+        </AccordionItem>
+    {/each}
+</div>
