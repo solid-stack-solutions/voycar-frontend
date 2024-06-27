e3496a8b0a5d2b94c6ee17232c0ce0b8d213e5aa
Janniix <jannik.hennicke@gmail.com>
2024-06-28 00:01:14 +0200

    feat: add working update of user data, add notification toast if update was successful

--- src/routes/user/userComponents/userData.svelte
+++ src/routes/user/userComponents/userData.svelte
@@ -1,9 +1,84 @@
 <script>
+    // Import 🐦
+    import { ConstantBackoff, handleAll, retry } from "cockatiel";
+    import { getToastStore } from "@skeletonlabs/skeleton";
+
+    // Import backend urls
+    import { urls } from "$lib/util.js";
     // Parent data exports
     export let personalData;
 
     // Definitions
+    const toastStore = getToastStore();
+
+
     let formEditEnabled = false;
+
+    let firstNameReference;
+    let lastNameReference;
+    let phonenumberReference;
+    let streetReference;
+    let houseNumberReference;
+    let cityReference;
+    let postalCodeReference;
+
+    // Policy for fetching
+    const retryPolicy = retry(handleAll, {
+        maxAttempts: 3, // Try 3 times
+        backoff: new ConstantBackoff(50), // Wait 50ms after each try
+    });
+
+    // Toast Settings
+    const toast = {
+        message: "Deine Daten wurden erfolgreich aktualisiert",
+        hideDismiss: true, // Hide the dismiss button on toast
+        timeout: 3000, // Auto dismiss toast after 3 seconds
+        background: "variant-filled-secondary",
+    };
+
+    function collectData(){
+        return  {
+        firstName:  firstNameReference.value  == '' ? personalData.firstName : firstNameReference.value,
+        lastName: lastNameReference.value  == '' ? personalData.lastName : lastNameReference.value,
+        street: streetReference.value  == '' ? personalData.street : streetReference.value,
+        houseNumber: houseNumberReference.value  == '' ? personalData.houseNumber : houseNumberReference.value,
+        postalCode: postalCodeReference.value  == '' ? personalData.postalCode : postalCodeReference.value,
+        city: cityReference.value == '' ? personalData.city : cityReference.value,
+        country: personalData.country,
+        birthDate: personalData.birthDate,
+        birthPlace: personalData.birthPlace,
+        phoneNumber: phonenumberReference.value  == '' ? personalData.phoneNumber : phonenumberReference.value,
+        };
+    }
+
+    async function updateUserData() {
+        if (formEditEnabled) {
+            formEditEnabled = false;
+        }
+        try {
+            const mybody = collectData();
+            console.log(JSON.stringify(mybody));
+            const response = await retryPolicy.execute(() =>
+                fetch(
+                    new Request(urls.put.newPersonalPlan, {
+                        method: "PUT",
+                        credentials: "include",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify(mybody),
+                    }),
+                ),
+            );
+            if (response.ok) {
+                toastStore.trigger(toast);
+            } else {
+                // Login failed on the backend side e.g. because credentials didn't match or account doesn't exists
+                throw new Error("Update failed");
+            }
+        } catch (err) {
+        }
+    }
 </script>
 
 <div class="h-full p-2">
@@ -23,6 +98,7 @@
                             type="text"
                             name="inputField"
                             placeholder={personalData.firstName}
+                            bind:this={firstNameReference}
                         />
                     </label>
                 </form>
@@ -41,6 +117,7 @@
                             type="text"
                             name="inputField"
                             placeholder={personalData.lastName}
+                            bind:this={lastNameReference}
                         />
                     </label>
                 </form>
@@ -57,6 +134,7 @@
                             type="text"
                             name="inputField"
                             placeholder={personalData.phoneNumber}
+                            bind:this={phonenumberReference}
                         />
                     </label>
                 </form>
@@ -73,6 +151,7 @@
                             type="text"
                             name="inputField"
                             placeholder={personalData.street}
+                            bind:this={streetReference}
                         />
                     </label>
                 </form>
@@ -89,6 +168,7 @@
                             type="text"
                             name="inputField"
                             placeholder={personalData.houseNumber}
+                            bind:this={houseNumberReference}
                         />
                     </label>
                 </form>
@@ -107,6 +187,7 @@
                             type="text"
                             name="inputField"
                             placeholder={personalData.city}
+                            bind:this={cityReference}
                         />
                     </label>
                 </form>
@@ -123,6 +204,7 @@
                             type="text"
                             name="inputField"
                             placeholder={personalData.postalCode}
+                            bind:this={postalCodeReference}
                         />
                     </label>
                 </form>
@@ -147,11 +229,7 @@
         <button
             type="button"
             class="variant-filled-primary btn btn-md"
-            on:click={() => {
-                if (formEditEnabled) {
-                    formEditEnabled = false;
-                }
-            }}
+            on:click={updateUserData}
         >
             <!-- ToDo logic for update button-->
             Aktualisieren
