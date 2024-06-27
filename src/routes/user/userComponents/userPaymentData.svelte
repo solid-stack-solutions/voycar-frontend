b1b6f01f338f46cee9edd75979fbe49b388c0174
Janniix <jannik.hennicke@gmail.com>
2024-06-27 18:27:36 +0200

    feat: change position of buttons, add not allowed cursor hover to radio buttons

--- src/routes/user/userComponents/userPaymentData.svelte
+++ src/routes/user/userComponents/userPaymentData.svelte
@@ -4,16 +4,16 @@
     // ToDo logic for backend fetch
 </script>
 
-<div>
-    <div style="flex grid-rows-2">
-        Zahlungsmittel:
+<div class="h-full p-2 relative">
+    <div class="flex flex-col h-full">
+        <p>Zahlungsmittel:</p>
         <!-- ToDo logic for show/hide payment data -->
         <!-- ToDo logic to only show available payment methods instead of hardcoded text -->
         <div class="space-y-2">
             <label class="flex items-center space-x-2">
                 <input
                     disabled={!formEditEnabled}
-                    class="radio"
+                    class="radio {formEditEnabled ? '':'hover:cursor-not-allowed'}"
                     type="radio"
                     checked
                     name="radio-direct"
@@ -24,7 +24,7 @@
             <label class="flex items-center space-x-2">
                 <input
                     disabled={!formEditEnabled}
-                    class="radio"
+                    class="radio {formEditEnabled ? '':'hover:cursor-not-allowed'}"
                     type="radio"
                     name="radio-direct"
                     value="2"
@@ -34,7 +34,7 @@
             <label class="flex items-center space-x-2">
                 <input
                     disabled={!formEditEnabled}
-                    class="radio"
+                    class="radio {formEditEnabled ? '':'hover:cursor-not-allowed'}"
                     type="radio"
                     name="radio-direct"
                     value="3"
@@ -43,29 +43,31 @@
             </label>
         </div>
     </div>
+    <div class="absolute right-2 bottom-8">
     <!-- Buttons -->
-    <div dir="rtl" class="height-auto">
-        <button type="button" class="variant-filled-error btn btn-md">
-            <!-- ToDo logic for delete button -->
-            Löschen
-        </button>
-        <button
-            type="button"
-            class="variant-filled-warning btn btn-md"
-            on:click={() => (formEditEnabled = !formEditEnabled)}
-        >
-            Bearbeiten
-            {#if formEditEnabled}
-                verlassen
-            {/if}
-            <img src="/editIcon.svg" alt="edit icon" />
-        </button>
-        {#if formEditEnabled}
-            <button type="button" class="variant-filled-success btn btn-md">
-                <!-- ToDo logic for update button -->
-                Aktualisieren
-                <img src="/saveIcon.svg" alt="save icon" />
+        <div dir="rtl" class="">
+            <button type="button" class="variant-filled-error btn btn-md">
+                <!-- ToDo logic for delete button -->
+                Löschen
             </button>
-        {/if}
+            <button
+                type="button"
+                class="variant-filled-warning btn btn-md"
+                on:click={() => (formEditEnabled = !formEditEnabled)}
+            >
+                Bearbeiten
+                {#if formEditEnabled}
+                    verlassen
+                {/if}
+                <img src="/editIcon.svg" alt="edit icon" />
+            </button>
+            {#if formEditEnabled}
+                <button type="button" class="variant-filled-success btn btn-md">
+                    <!-- ToDo logic for update button -->
+                    Aktualisieren
+                    <img src="/saveIcon.svg" alt="save icon" />
+                </button>
+            {/if}
+        </div>
     </div>
 </div>
