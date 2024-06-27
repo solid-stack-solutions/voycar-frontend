ae727bb2aa8278970fdc8badc0e961c1c3750457
Janniix <jannik.hennicke@gmail.com>
2024-06-27 09:57:23 +0200

    feat: hardcode displayed user data fields

--- src/routes/user/userComponents/userData.svelte
+++ src/routes/user/userComponents/userData.svelte
@@ -1,7 +1,6 @@
 <script>
     // Parent data exports
-    export let userKeys;
-    export let userInfo;
+    export let userData;
 
     // Definitions
     let formEditEnabled = false;
@@ -9,32 +8,106 @@
 
 <div class="p-2">
     <table>
-        {#each userKeys as key, i}
-            <!-- Only show rows with information available -->
-            {#if userInfo[i] != null}
-                <!-- Diplays all the information of the specified user in the url params 
-                (doesnt sanitize the data its given; means query data has to be clean) -->
-                <tr>
-                    <th class="text-left">
-                        <span class="mr-5">{key.toLowerCase()}</span>
-                    </th>
-                    <td class="w-full">
-                        <form id="form">
-                            <label class="label">
-                                <input
-                                    disabled={!formEditEnabled}
-                                    class="form--disabled input variant-form-material"
-                                    type="text"
-                                    name="inputField"
-                                    placeholder={userInfo[i]}
-                                    id="input_{key}"
-                                />
-                            </label>
-                        </form>
-                    </td>
-                </tr>
-            {/if}
-        {/each}
+        <!-- Diplays all the information of the specified user in the url params 
+        (doesnt sanitize the data its given; means query data has to be clean) -->
+        <tr>
+            <th class="text-left">
+                <span class="mr-5">Vorname:</span>
+            </th>
+            <td class="w-full">
+                <form id="form">
+                    <label class="label">
+                        <input
+                            disabled={!formEditEnabled}
+                            class="form--disabled input variant-form-material"
+                            type="text"
+                            name="inputField"
+                            placeholder={userData.firstName}
+                        />
+                    </label>
+                </form>
+            </td>
+        </tr>
+        <tr>
+            <th class="text-left">
+                <span class="mr-5">Nachname:</span>
+            </th>
+            <td class="w-full">
+                <form id="form">
+                    <label class="label">
+                        <input
+                            disabled={!formEditEnabled}
+                            class="form--disabled input variant-form-material"
+                            type="text"
+                            name="inputField"
+                            placeholder={userData.lastName}
+                        />
+                    </label>
+                </form>
+            </td>
+        </tr>
+        <tr>
+            <th class="text-left">
+                <span class="mr-5">Anschrift:</span>
+            </th>
+            <td>
+                <form id="form">
+                    <label class="label">
+                        <input
+                            disabled={!formEditEnabled}
+                            class="form--disabled input variant-form-material"
+                            type="text"
+                            name="inputField"
+                            placeholder={userData.street}
+                        />
+                    </label>
+                </form>
+            </td>
+            <td>
+                <form id="form">
+                    <label class="label">
+                        <input
+                            disabled={!formEditEnabled}
+                            class="form--disabled input variant-form-material"
+                            type="text"
+                            name="inputField"
+                            placeholder={userData.housenumber}
+                        />
+                    </label>
+                </form>
+            </td>
+        </tr>
+        <tr>
+            <th class="text-left">
+                <span class="mr-5">Ort:</span>
+            </th>
+            <td>
+                <form id="form">
+                    <label class="label">
+                        <input
+                            disabled={!formEditEnabled}
+                            class="form--disabled input variant-form-material"
+                            type="text"
+                            name="inputField"
+                            placeholder={userData.city}
+                        />
+                    </label>
+                </form>
+            </td>
+            <td>
+                <form id="form">
+                    <label class="label">
+                        <input
+                            disabled={!formEditEnabled}
+                            class="form--disabled input variant-form-material"
+                            type="text"
+                            name="inputField"
+                            placeholder={userData.postalCode}
+                        />
+                    </label>
+                </form>
+            </td>
+        </tr>
     </table>
 </div>
 <!-- Buttons -->
@@ -51,7 +124,7 @@
         <img src="/editIcon.svg" alt="edit icon" />
     </button>
     {#if formEditEnabled}
-        <button type="button" class="variant-filled-success btn btn-md">
+        <button type="button" class="variant-filled-primary btn btn-md">
             <!-- ToDo logic for update button-->
             Aktualisieren
             <img src="/saveIcon.svg" alt="save icon" />
