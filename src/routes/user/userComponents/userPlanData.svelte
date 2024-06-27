17c18521542fe6057be3b2dfdf1141d65fb6d25c
Janniix <jannik.hennicke@gmail.com>
2024-06-27 19:17:19 +0200

    feat: add selctor for plan

--- src/routes/user/userComponents/userPlanData.svelte
+++ src/routes/user/userComponents/userPlanData.svelte
@@ -1,20 +1,38 @@
 <script>
+    import { goto } from "$app/navigation";
     // Definitions
-    export let userID;
-    // ToDo logic for backend fetch
+    export let personalData;
+
+    let formEditEnabled = false;
 
     // ToDo maybe show possible plan and option to change current one
 </script>
 
 <div class="relative h-full">
     <div class="flex flex-row space-x-2">
-        <p>Aktueller Tarif:</p>
-        <div>{userID}</div>
+        <p class="self-center">Aktueller Tarif:</p>
+        <select 
+            class="select w-2/4" 
+            size="1" 
+            value="{personalData.planName}"
+            disabled = {!formEditEnabled}
+            >
+            <option value="basic">Basis</option>
+            <option value="reduced">Reduziert</option>
+            <option value="exclusive">Exklusiv</option>
+        </select>
     </div>
     <div class="absolute bottom-6 right-2">
-        <button class="variant-filled-warning btn btn-md">
-            <img src="/modifyPlan.svg" alt="" />
-            Tarif ändern
-        </button>
+        <button
+                type="button"
+                class="variant-filled-warning btn btn-md"
+                on:click={() => (formEditEnabled = !formEditEnabled)}
+                >
+                <img src="/editIcon.svg" alt="edit icon" />
+                Bearbeiten
+                {#if formEditEnabled}
+                    verlassen
+                {/if}
+            </button>
     </div>
 </div>
