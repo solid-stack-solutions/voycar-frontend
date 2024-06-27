2ff375ea58d2474acde254274c8a3b483ae44a6a
Janniix <jannik.hennicke@gmail.com>
2024-06-28 01:13:09 +0200

    feat: add reload button to reload page after updating data

--- src/routes/user/userComponents/userData.svelte
+++ src/routes/user/userComponents/userData.svelte
@@ -10,9 +10,9 @@
 
     // Definitions
     const toastStore = getToastStore();
-
-
+    
     let formEditEnabled = false;
+    let needReload = false;
 
     let firstNameReference;
     let lastNameReference;
@@ -81,137 +81,145 @@
     }
 </script>
 
-<div class="h-full p-2">
-    <table class="table-fixed border-separate border-spacing-x-4">
-        <!-- Diplays all the information of the specified user in the url params 
-        (doesnt sanitize the data its given; means query data has to be clean) -->
-        <tr>
-            <th class="text-left">
-                <span class="mr-5">Vorname:</span>
-            </th>
-            <td>
-                <form id="form">
-                    <label class="label">
-                        <input
-                            disabled={!formEditEnabled}
-                            class="form--disabled input variant-form-material"
-                            type="text"
-                            name="inputField"
-                            placeholder={personalData.firstName}
-                            bind:this={firstNameReference}
-                        />
-                    </label>
-                </form>
-            </td>
-        </tr>
-        <tr>
-            <th class="text-left">
-                <span class="mr-5">Nachname:</span>
-            </th>
-            <td class="w-full">
-                <form id="form">
-                    <label class="label">
-                        <input
-                            disabled={!formEditEnabled}
-                            class="form--disabled input variant-form-material"
-                            type="text"
-                            name="inputField"
-                            placeholder={personalData.lastName}
-                            bind:this={lastNameReference}
-                        />
-                    </label>
-                </form>
-            </td>
-        </tr>
-        <tr>
-            <th class="text-left"> Telefonnummer: </th>
-            <td>
-                <form id="form">
-                    <label class="label">
-                        <input
-                            disabled={!formEditEnabled}
-                            class="form--disabled input variant-form-material"
-                            type="text"
-                            name="inputField"
-                            placeholder={personalData.phoneNumber}
-                            bind:this={phonenumberReference}
-                        />
-                    </label>
-                </form>
-            </td>
-        </tr>
-        <tr>
-            <th class="text-left"> Straße: </th>
-            <td>
-                <form id="form">
-                    <label class="label">
-                        <input
-                            disabled={!formEditEnabled}
-                            class="form--disabled input variant-form-material"
-                            type="text"
-                            name="inputField"
-                            placeholder={personalData.street}
-                            bind:this={streetReference}
-                        />
-                    </label>
-                </form>
-            </td>
-        </tr>
-        <tr>
-            <th class="text-left"> Hausnummer: </th>
-            <td>
-                <form id="form">
-                    <label class="label">
-                        <input
-                            disabled={!formEditEnabled}
-                            class="form--disabled input variant-form-material"
-                            type="text"
-                            name="inputField"
-                            placeholder={personalData.houseNumber}
-                            bind:this={houseNumberReference}
-                        />
-                    </label>
-                </form>
-            </td>
-        </tr>
-        <tr>
-            <th class="text-left">
-                <span class="mr-5">Ort:</span>
-            </th>
-            <td>
-                <form id="form">
-                    <label class="label">
-                        <input
-                            disabled={!formEditEnabled}
-                            class="form--disabled input variant-form-material"
-                            type="text"
-                            name="inputField"
-                            placeholder={personalData.city}
-                            bind:this={cityReference}
-                        />
-                    </label>
-                </form>
-            </td>
-        </tr>
-        <tr>
-            <th class="text-left"> Postleitzahl: </th>
-            <td>
-                <form id="form">
-                    <label class="label">
-                        <input
-                            disabled={!formEditEnabled}
-                            class="form--disabled input variant-form-material"
-                            type="text"
-                            name="inputField"
-                            placeholder={personalData.postalCode}
-                            bind:this={postalCodeReference}
-                        />
-                    </label>
-                </form>
-            </td>
-        </tr>
-    </table>
-</div>
+<div class="h-full space-y-2">
+    {#if needReload}
+       <div class="flex flex-col items-center justify-center h-full">
+            <h3 class="h3">
+                Daten neu Laden
+            </h3>
+            <button class="btn variant-filled-primary"  on:click={() => (location.reload())}>Reload</button>
+       </div> 
+    {:else}
+    <div>
+        <table class="table-fixed border-separate border-spacing-x-4">
+            <tr>
+                <th class="text-left">
+                    <span class="mr-5">Vorname:</span>
+                </th>
+                <td>
+                    <form id="form">
+                        <label class="label">
+                            <input
+                                disabled={!formEditEnabled}
+                                class="form--disabled input variant-form-material"
+                                type="text"
+                                name="inputField"
+                                placeholder={personalData.firstName}
+                                bind:this={firstNameReference}
+                            />
+                        </label>
+                    </form>
+                </td>
+            </tr>
+            <tr>
+                <th class="text-left">
+                    <span class="mr-5">Nachname:</span>
+                </th>
+                <td class="w-full">
+                    <form id="form">
+                        <label class="label">
+                            <input
+                                disabled={!formEditEnabled}
+                                class="form--disabled input variant-form-material"
+                                type="text"
+                                name="inputField"
+                                placeholder={personalData.lastName}
+                                bind:this={lastNameReference}
+                            />
+                        </label>
+                    </form>
+                </td>
+            </tr>
+            <tr>
+                <th class="text-left"> Telefonnummer: </th>
+                <td>
+                    <form id="form">
+                        <label class="label">
+                            <input
+                                disabled={!formEditEnabled}
+                                class="form--disabled input variant-form-material"
+                                type="text"
+                                name="inputField"
+                                placeholder={personalData.phoneNumber}
+                                bind:this={phonenumberReference}
+                            />
+                        </label>
+                    </form>
+                </td>
+            </tr>
+            <tr>
+                <th class="text-left"> Straße: </th>
+                <td>
+                    <form id="form">
+                        <label class="label">
+                            <input
+                                disabled={!formEditEnabled}
+                                class="form--disabled input variant-form-material"
+                                type="text"
+                                name="inputField"
+                                placeholder={personalData.street}
+                                bind:this={streetReference}
+                            />
+                        </label>
+                    </form>
+                </td>
+            </tr>
+            <tr>
+                <th class="text-left"> Hausnummer: </th>
+                <td>
+                    <form id="form">
+                        <label class="label">
+                            <input
+                                disabled={!formEditEnabled}
+                                class="form--disabled input variant-form-material"
+                                type="text"
+                                name="inputField"
+                                placeholder={personalData.houseNumber}
+                                bind:this={houseNumberReference}
+                            />
+                        </label>
+                    </form>
+                </td>
+            </tr>
+            <tr>
+                <th class="text-left">
+                    <span class="mr-5">Ort:</span>
+                </th>
+                <td>
+                    <form id="form">
+                        <label class="label">
+                            <input
+                                disabled={!formEditEnabled}
+                                class="form--disabled input variant-form-material"
+                                type="text"
+                                name="inputField"
+                                placeholder={personalData.city}
+                                bind:this={cityReference}
+                            />
+                        </label>
+                    </form>
+                </td>
+            </tr>
+            <tr>
+                <th class="text-left"> Postleitzahl: </th>
+                <td>
+                    <form id="form">
+                        <label class="label">
+                            <input
+                                disabled={!formEditEnabled}
+                                class="form--disabled input variant-form-material"
+                                type="text"
+                                name="inputField"
+                                placeholder={personalData.postalCode}
+                                bind:this={postalCodeReference}
+                            />
+                        </label>
+                    </form>
+                </td>
+            </tr>
+        </table>
+    </div>
+   
 <!-- Buttons -->
 <div dir="rtl">
     <button
