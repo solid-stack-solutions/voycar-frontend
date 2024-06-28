a6822a73518776f1c3adeee5a9858bc92956eb9e
Janniix <jannik.hennicke@gmail.com>
2024-06-28 20:02:50 +0200

    feat: add feature information toast on payment info update and delete

--- src/routes/user/userComponents/userPaymentData.svelte
+++ src/routes/user/userComponents/userPaymentData.svelte
@@ -1,17 +1,34 @@
 <script>
+    import { getToastStore } from "@skeletonlabs/skeleton";
+    
     // Definitions
+    const toastStore = getToastStore();
+
     let formEditEnabled = false;
-    // export let paymentName;
-    // ToDo logic for backend fetch
+    export let personalData;
+    let paymentName = personalData.paymentName;
+
+    const infoToast = {
+        message: "Dieses Feature ist zur Zeit noch nicht verfügbar",
+        hideDismiss: true, // Hide the dismiss button on toast
+        timeout: 3000, // Auto dismiss toast after 3 seconds
+        background: "variant-filled-primary",
+    };
 
 
+    function updatePaymentData(){
+        formEditEnabled = false;
+        toastStore.trigger(infoToast);
+    }
+
+    function deletePaymentData(){
+        toastStore.trigger(infoToast);
+    }
 </script>
 
 <div class="relative h-full p-2">
     <div class="flex h-full flex-col">
         <p>Zahlungsmittel:</p>
-        <!-- ToDo logic for show/hide payment data -->
-        <!-- ToDo logic to only show available payment methods instead of hardcoded text -->
         <div class="space-y-2">
             <label class="flex items-center space-x-2">
                 <input
@@ -55,7 +72,7 @@
     <div class="absolute bottom-6 right-2">
         <!-- Buttons -->
         <div dir="rtl" class="">
-            <button type="button" class="variant-filled-error btn btn-md">
+            <button type="button" class="variant-filled-error btn btn-md" on:click={deletePaymentData}>
                 <!-- ToDo logic for delete button -->
                 Löschen
             </button>
@@ -71,18 +88,14 @@
                 <img src="/editIcon.svg" alt="edit icon" />
             </button>
             {#if formEditEnabled}
-                <button 
-                type="button" 
-                class="variant-filled-success btn btn-md"
-                on:click={() => {
-                    if (formEditEnabled) {
-                        formEditEnabled = false;
-                    }
-                    }}>
-                    <!-- ToDo logic for update button -->
-                    Aktualisieren
-                    <img src="/saveIcon.svg" alt="save icon" />
-                </button>
+                <button
+                type="button"
+                class="variant-filled-primary btn btn-md"
+                on:click={updatePaymentData}
+            >
+                Aktualisieren
+                <img src="/saveIcon.svg" alt="save icon" />
+            </button>
             {/if}
         </div>
     </div>
