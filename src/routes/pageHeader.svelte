e95b1d782f1b63723cecf3d31a15e55bb5d01f7a
Janniix <jannik.hennicke@gmail.com>
2024-06-29 01:21:38 +0200

    style: run format

--- src/routes/pageHeader.svelte
+++ src/routes/pageHeader.svelte
@@ -57,7 +57,16 @@
             <div />
         </svelte:fragment>
         <svelte:fragment slot="default">
-            <a href="/" on:click|preventDefault={() => {if(window.location.pathname == "/") {location.reload();}else{window.location.href = "/"}}} >
+            <a
+                href="/"
+                on:click|preventDefault={() => {
+                    if (window.location.pathname == "/") {
+                        location.reload();
+                    } else {
+                        window.location.href = "/";
+                    }
+                }}
+            >
                 <!-- Voycar Logo -->
                 <img
                     src="/logo-full-white.svg"
