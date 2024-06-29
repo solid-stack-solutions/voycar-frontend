83d0a0023c870e7cb0e63775b178098604c32303
van Rijsinge <nilspatrick.vanrijsinge@btc-ag.com>
2024-06-29 13:38:58 +0200

    feat: use translation

--- src/routes/user/userComponents/userPlanData.svelte
+++ src/routes/user/userComponents/userPlanData.svelte
@@ -4,7 +4,7 @@
     import { onMount } from "svelte";
 
     // Import backend urls and fetch function
-    import { urls, tryFetchingRestricted } from "$lib/util.js";
+    import { urls, tryFetchingRestricted, translatePlanName } from "$lib/util.js";
 
     // Definitions
     const toastStore = getToastStore();
@@ -126,7 +126,7 @@
                 bind:this={planReference}
             >
                 {#each planData as plan}
-                    <option value={plan.id}>{plan.name}</option>
+                    <option value={plan.id}>{translatePlanName(plan.name)}</option>
                 {/each}
             </select>
         </div>
