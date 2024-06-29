0def2a7d051aac6101fbcb25b51cc521c97543b7
Janniix <53787111+Janniix@users.noreply.github.com>
2024-06-29 12:16:15 +0200

    feat: apply suggestion to use a optimized resolve function
    Co-authored-by: julius-boettger <julius.btg@proton.me>

--- src/routes/user/userComponents/userPlanData.svelte
+++ src/routes/user/userComponents/userPlanData.svelte
@@ -47,12 +47,11 @@
     }
 
     function resolvePlanNameToPlanId(planName) {
-        for (let index = 0; index < planData.length; index++) {
-            if (planData[index].name == planName) {
-                return planData[index].id;
-            }
+        let plan = planData.find(plan => plan.name == planName);
+        if (plan === undefined) {
+            throw new Error("Planname is not in backend plan list");
         }
-        throw new Error("Planname is not in backend plan list");
+        return plan.id; 
     }
 
     function resetFieldOnCancel() {
