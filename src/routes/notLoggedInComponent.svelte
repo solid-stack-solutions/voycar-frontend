2600cec0c818627510de843c704ccca38e8df04b
Janniix <jannik.hennicke@gmail.com>
2024-06-30 23:09:07 +0200

    feat: create not logged in component

---
+++ src/routes/notLoggedInComponent.svelte
@@ -0,0 +1,5 @@
+<div class="flex h-[70vh] flex-col items-center justify-center">
+    <div class="border-md rounded-md bg-surface-700 p-4">
+        <h1 class="h1 text-center">Diese Seite steht nur angemeldeten Nutzern zur Verfügung!</h1>
+    </div>
+</div>
