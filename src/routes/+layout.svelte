dc13ba005c7c13e642129361bf712501629d49a3
Janniix <jannik.hennicke@gmail.com>
2024-07-01 00:09:22 +0200

    feat: remove obsolete imports

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,43 +13,17 @@
     storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
     import { initializeStores, Toast } from "@skeletonlabs/skeleton";
     import PageHeader from "./pageHeader.svelte";
-    import { onMount } from "svelte";
-    import { urls, tryFetchingRestricted} from "$lib/util.js";
     initializeStores();
 
-    let loggedIn = new Promise((resolve, reject) => {});
+    export let data;
     // Runs as soon as the component is mounted
-    onMount(async () => {
-        loggedIn = new Promise(async (resolve, reject) => {
-            try {
-                console.log("logged");
-                // Fetch backend to check if user is signed in
-                const response = await tryFetchingRestricted(urls.get.isLoggedIn);
-                if (response.ok) {
-                    resolve(await response.json());
-                } else {
-                    throw new Error("User isnt logged in");
-                }
-            } catch (err) {
-                reject(err);
-            }
-        });
-    });
 </script>
 
 <Toast />
 <div>
-    {#await loggedIn}
-        <div/>
-    {:then loggedIn} 
-        <PageHeader loggedIn={true}/>
+    {data.loggedIn}
+        <PageHeader loggedIn={data.loggedIn}/>
         <div class="h-fit p-4">
-            <slot loggedIn={true}/>
+            <slot />
         </div>
-    {:catch err}
-    <PageHeader loggedIn={false}/>
-    <div class="h-fit p-4">
-        <slot loggedIn={false}/>
-    </div>
-    {/await}
 </div>
