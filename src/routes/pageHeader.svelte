fffdcb9aeec070a954427e730c875fad313c95a4
Janniix <jannik.hennicke@gmail.com>
2024-06-30 20:30:38 +0200

    feat: run format

--- src/routes/pageHeader.svelte
+++ src/routes/pageHeader.svelte
@@ -79,13 +79,16 @@
                 <div class="flex flex-row space-x-2">
                     <button
                         class="variant-ringed-surface btn"
-                        on:click={() => {goto("/register");}}>Registrieren</button
+                        on:click={() => {
+                            goto("/register");
+                        }}>Registrieren</button
                     >
                     <button
                         class="variant-ringed-primary btn"
-                        on:click={() => {goto("/login");}}>Anmelden</button
+                        on:click={() => {
+                            goto("/login");
+                        }}>Anmelden</button
                     >
-                    
                 </div>
             {/if}
         </svelte:fragment>
