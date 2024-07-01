deb9ed92dded2e421e04603436ff469a1b2463ca
Janniix <jannik.hennicke@gmail.com>
2024-07-01 21:29:27 +0200

    feat: change footer bg to header bg

--- src/routes/footer.svelte
+++ src/routes/footer.svelte
@@ -1,5 +1,5 @@
 <div
-    class="fixed bottom-0 flex w-full flex-row items-end justify-center bg-surface-600"
+    class="fixed bottom-0 flex w-full flex-row items-end justify-center bg-surface-800"
 >
     <img src="/telephoneIcon.svg" alt="telephone icon" class="scale-75" />
     <p class="mr-4">+49 913 914161914</p>
