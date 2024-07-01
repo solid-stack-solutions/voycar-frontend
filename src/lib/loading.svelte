88e0d3a45468383ae72cbabda4bb2cad7ffa726e
NilsPvR <nilspvr@pm.me>
2024-07-01 16:38:56 +0200

    feat: add shared loading component

---
+++ src/lib/loading.svelte
@@ -0,0 +1,14 @@
+<script>
+    // Framework imports
+    import { ProgressRadial } from "@skeletonlabs/skeleton";
+</script>
+
+<div class="flex h-[70vh] flex-col items-center justify-center space-y-4">
+    <ProgressRadial
+        stroke={60}
+        meter="stroke-primary-500"
+        track="stroke-primary-500/30"
+        strokeLinecap="butt"
+        width="w-20"
+    />
+</div>
