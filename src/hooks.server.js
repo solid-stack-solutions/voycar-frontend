0e3b5c36ab6af826261dc8596008eece1e34bd7b
Janniix <jannik.hennicke@gmail.com>
2024-07-01 14:38:35 +0200

    doc: add comment

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -2,6 +2,7 @@ import { redirect } from "@sveltejs/kit";
 export async function handle({ event, resolve }) {
     const response = await resolve(event);
     if (response.status === 404) {
+        // Redirect client for non-existing pages to landing page
         throw redirect(302, "/");
     }
     return response;
