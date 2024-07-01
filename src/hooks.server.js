7e591657df4eb60cb8d4db39e222675952f5398b
Janniix <jannik.hennicke@gmail.com>
2024-07-01 14:36:54 +0200

    style: run format

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,8 +1,8 @@
 import { redirect } from "@sveltejs/kit";
 export async function handle({ event, resolve }) {
-	const response = await resolve(event);
-    if (response.status === 404){
-        throw redirect(302, '/');
+    const response = await resolve(event);
+    if (response.status === 404) {
+        throw redirect(302, "/");
     }
-	return response;
+    return response;
 }
