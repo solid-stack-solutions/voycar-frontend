c392155ddbebffbc1d58fc2b49f8b9c3a9cb26d7
kevin-lingk <klingk.msg@gmail.com>
2024-07-14 01:24:55 +0200

    feat: some testing with the landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,46 @@
 <!-- Landing page -->
 <script>
+    import { goto } from "$app/navigation";
+    import {loggedIn} from "$lib/stores/loggedIn.js";
 </script>
 
 <svelte:head>
     <title>Voycar - Startseite</title>
 </svelte:head>
 
-<div class="flex h-[70vh] flex-col items-center justify-center">
-    <div class="border-md rounded-md bg-surface-700 p-4">
-        <h1 class="h1 text-center">Willkommen bei Voycar</h1>
+
+<section class="flex h-[70vh] flex-col items-center justify-center">
+    <div class="grid max-w-screen-xl lg:grid-cols-12">
+        <div class="place-self-center lg:col-span-7">
+            <h1 class=" h1 mb-8 xl:text-6xl ">Haben wir nen tollen Spruch?</h1>
+            <p class=" h4 mb-4">
+                Hier sollte am besten auch noch irgendwas tolles stehen.</p>
+            <div>
+                {#if !$loggedIn}
+                    <div class="flex flex-row space-x-2">
+                        <button
+                                class="variant-ringed-surface btn"
+                                on:click={() => {
+                            goto("/register");
+                        }}>Registrieren</button
+                        >
+                        <button
+                                class="variant-ringed-primary btn"
+                                on:click={() => {
+                            goto("/login");
+                        }}>Anmelden</button
+                        >
+                    </div>
+                {/if}
+            </div>
+        </div>
+        <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+            <img
+                    src="/logo-full-white.svg"
+                    alt="logo"
+                    class="w-500 scale-700"
+            />
+        </div>
     </div>
-</div>
+</section>
+
